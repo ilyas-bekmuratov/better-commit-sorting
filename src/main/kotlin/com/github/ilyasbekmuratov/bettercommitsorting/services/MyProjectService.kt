1511e88e2ef17062e13f7db7db4801b96912b174
GitHub Action <action@github.com>
2026-03-10 04:09:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/ilyasbekmuratov/bettercommitsorting/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ilyasbekmuratov/bettercommitsorting/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ilyasbekmuratov.bettercommitsorting.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ilyasbekmuratov.bettercommitsorting.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
